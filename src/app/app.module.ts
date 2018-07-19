557666a455a54064b57275f5ad8a2dee7237254d
Manny <coachmannytw@gmail.com>
2018-07-18 23:31:27 -0600

    updated npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     CribListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { cribs } from './../data/cribs'
 
+
 @Component({
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
